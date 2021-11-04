d2078ad2ebbd881a71b79d84bf56905b7d0645ba
luka <lukawivilashvili@gmail.com>
2021-11-04 15:34:45 +0400

    added and initialized hilt

--- app/src/main/java/com/luka/themoviedb/MainActivity.kt
+++ app/src/main/java/com/luka/themoviedb/MainActivity.kt
@@ -7,8 +7,9 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.luka.themoviedb.adapters.ViewPagerAdapter
 import com.luka.themoviedb.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewPager: ViewPager2
---
+++ app/src/main/java/com/luka/themoviedb/di/AppClass.kt
@@ -0,0 +1,8 @@
+package com.luka.themoviedb.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppClass:Application() {
+}
